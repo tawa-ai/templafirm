1853dbb4e4c98c279589720e4195ed25bfec7f41
MaxWeirz <willettisaak@gmail.com>
2025-05-02 12:38:11 -0700

    Add Format Step

--- templafirm/gke/gke_provider.py
+++ templafirm/gke/gke_provider.py
@@ -6,14 +6,18 @@ from templafirm.gke import templates
 
 
 class GKEProvider(Provider):
+    """Template provider for GKE. Abstraction on top of file path provided in package."""
+
     def __init__(self) -> None:
         self._localized_assets_pathway = self._get_localized_assets_path()
         super().__init__(self._localized_assets_pathway)
 
     def template_directory_path(self) -> str:
+        """Return the template directory relative to python env."""
         template_module_file_path = pathlib.Path(os.path.abspath(templates.__file__))
         return str(template_module_file_path.parent)
 
     def _get_localized_assets_path(self) -> str:
+        """Get the localized assets path."""
         gke_provider_meta_path = os.path.join(self.template_directory_path(), "gke_provider_meta.yaml")
         return gke_provider_meta_path
